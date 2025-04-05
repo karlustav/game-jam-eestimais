d8acbe91e8e50ca43156fadded98b6a167da686b
Karl-Ustav Kõlar <karlustavkolar@Karl-Ustavs-MacBook-Air.local>
2025-04-05 17:36:29 +0300

    fixed static bomb

--- Assets/Scripts/StaticBomb.cs
+++ Assets/Scripts/StaticBomb.cs
@@ -5,21 +5,18 @@ using UnityEngine;
 public class StaticBomb : MonoBehaviour
 {
     public float plahvatusAeg = 5.0f;
-    IEnumerator Explosion() {
-        print("kohe teeb pauku");
-        yield return new WaitForSeconds(plahvatusAeg);
-        GameObject.Find("mangija").SendMessage("Explode", transform);
+
+    IEnumerator ExplosionLoop() {
+        while (true) {
+            print("kohe teeb pauku");
+            yield return new WaitForSeconds(plahvatusAeg);
+            GameObject.Find("mangija").SendMessage("Explode", transform);
+        }
     }
 
     void Start()
     {
- 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         print("hakkab pauku tegema");
-        StartCoroutine(Explosion());
+        StartCoroutine(ExplosionLoop());
     }
 }
