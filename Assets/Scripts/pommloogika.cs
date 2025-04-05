b8e3f2cbb71b6c344d188402589a55b7568ec46b
riotruij <rio.truija@ut.ee>
2025-04-05 15:58:57 +0300

    plahvatusaeg on parameeter

--- Assets/Scripts/pommloogika.cs
+++ Assets/Scripts/pommloogika.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class pommloogika : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float plahvatusAeg = 2.0f;
     void Start()
     {
         print("hakkab pauku tegema");
@@ -12,7 +13,7 @@ public class pommloogika : MonoBehaviour
     }
     IEnumerator Explosion() {
         print("kohe teeb pauku");
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(plahvatusAeg);
         GameObject.Find("mangija").SendMessage("Explode", transform);
         Destroy(gameObject);
     }
